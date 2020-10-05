66c17bcfec75a9b646729a1d66d1b57e2e5e7614
Jonathan Winwood <jonathan.winwood@gmail.com>
2020-10-05 20:17:06 +0100

    Changed from single to viewModel for the ViewModel in Koin

--- androidApp/src/main/java/com/example/marvelcomicsviewer/androidApp/characters/CharacterModule.kt
+++ androidApp/src/main/java/com/example/marvelcomicsviewer/androidApp/characters/CharacterModule.kt
@@ -2,10 +2,11 @@ package com.example.marvelcomicsviewer.androidApp.characters
 
 import com.example.marvelcomicsviewer.shared.characters.CharacterRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 @ExperimentalCoroutinesApi
 val characterModule = module {
     single { CharacterRepository() }
-    single { CharactersViewModel(get()) }
+    viewModel { CharactersViewModel(get()) }
 }
